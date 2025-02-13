e1c25805984cbc99d4e6f9b0d8fb911abc7d739b
𝕊𝕒𝕣𝕧𝕒𝕘𝕣𝕒 <163673574+sarvagra@users.noreply.github.com>
2025-02-13 14:06:04 +0530

    finished SQL

--- college.sql
+++ college.sql
@@ -3,14 +3,14 @@ CREATE DATABASE IF NOT EXISTS college;
 USE college;
 CREATE TABLE student(
     rollno INT PRIMARY KEY ,
-    name VARCHAR(50) DEFAULT UNIQUE, --takes only unique name entries
-    marks INT NOT NULL, --means the column cant hold null value,
-    grade VARCHAR(1) DEFAULT 'S', --sets default value as S
+    name VARCHAR(50) UNIQUE, -- takes only unique name entries
+    marks INT NOT NULL, -- means the column cant hold null value,
+    grade VARCHAR(1) DEFAULT 'S', -- sets default value as S
     city VARCHAR(25)
-)
+);
 INSERT INTO student(rollno, name, marks, grade, city)
 VALUES
-(101, "anil", 78, "C", "Pune"),
+(100, "anil", 78, "C", "Pune"),
 (101, "Gauri", 78, "C", "jaipur"),
 (102, "bhumika", 93, "A", "Mumbai"),
 (103, "chetan", 85, "B", "Mumbai"),
@@ -30,11 +30,31 @@ LIMIT 3;
 
 
 
--- Updaing
+-- Updating
 UPDATE student
 SET grade ="A"
-WHERE grade="C"
+WHERE 
 
 -- Deleting
 DELETE FROM student
 WHERE grade="F"
+
+-- ordering data
+SELECT * FROM student
+ORDER BY name DESC; -- DESC , ASC can be used to determine the order
+
+
+SELECT * FROM student
+ORDER BY marks,grade; -- first order by marks and then by grade
+
+SELECT * FROM student
+ORDER BY marks DESC
+LIMIT 3;  -- find the top 3 students
+
+-- filtering WHERE and IN keywords 
+ SELECT * FROM student
+ WHERE marks>80;
+
+ SELECT * FROM student
+ WHERE name IN ('chetan','gauri','anil')  -- note that it is not case sensitive
+
